87bf24a835ab5d72d6b62c757291a88a9a76fc49
Rob Connot <robertconnot@gmail.com>
2023-07-07 15:57:55 -0600

    refried and simplified

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '16px',
+        xs: '26px',
+        sm: '46px',
+        md: '44px',
+      },
+    },
+  },
+  plugins: [],
+}
